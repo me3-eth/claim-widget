5955e18d4d5e384b59f1947628b21be231cbb149
charchar <87212793+0xcharchar@users.noreply.github.com>
2022-06-08 01:05:49 +0100

    svelte usage working

--- examples/svelte/src/App.svelte
+++ examples/svelte/src/App.svelte
@@ -1,20 +1,22 @@
 <script>
-  import '../../../src/demo.css'
-  import { ClaimWidget } from '@me3/claim-widget'
-  import { mock } from '@depay/web3-mock'
+import '../../../src/demo.css'
+import '@me3/claim-widget'
+  //import { ClaimWidget } from '@me3/claim-widget'
+  //import { mock } from '@depay/web3-mock'
 
+  /*
   mock({
     blockchain: 'ethereum',
     accounts: {
       return: ['0xb25205ca60f964d45b30e969dc3f10a5de4ec3bc']
     }
   })
+  */
 </script>
 
 <main>
-  <ClaimWidget
+  <me3-claim-widget
     domain="me3.eth"
-    provider={window.ethereum}
     />
 
 </main>
