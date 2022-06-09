674cc5943b2d20e6ebf03d21b76a9bdc64a21ff5
charchar <87212793+0xcharchar@users.noreply.github.com>
2022-06-09 01:10:02 +0100

    mock requests to alchemy

--- examples/react/src/pages/HomePage.js
+++ examples/react/src/pages/HomePage.js
@@ -4,16 +4,65 @@ import { mock } from '@depay/web3-mock'
 import ReactMarkdown from 'react-markdown'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import readmePath from '../../README.md'
+import registrarAbi from '../../../../tests/Registrar.json'
+import faker from 'storybook-addon-mock/dist/utils/faker.js'
 
 import WebComponent from '@clubajax/react-web-component'
 import '@me3/claim-widget'
 
 mock({
   blockchain: 'ethereum',
-  accounts: {
-    return: ['0xb25205ca60f964d45b30e969dc3f10a5de4ec3bc']
-  }
+  transaction: {
+    to: '0x9f2daf90c4323b529c31a40520a5fa63eb601b84',
+    api: registrarAbi,
+    method: 'register',
+    params: ['0x868437061435f35898f8ed7fb95d62ca53b460f0bb9d1c6be3bfd796e38d8636', 'charchar', '0xb25205ca60f964d45b30e969dc3f10a5de4ec3bc', []]
+  },
+  accounts: { return: ['0xb25205ca60f964d45b30e969dc3f10a5de4ec3bc'] }
 })
+
+const metadataMock = {
+  url: 'https://eth-mainnet.alchemyapi.io/v2/abc123/getNFTMetadata?contractAddress=0x9759226B2F8ddEFF81583e244Ef3bd13AAA7e4A1&tokenId=',
+  method: 'GET',
+  status: 200,
+  response: req => {
+    const { searchParams } = req
+    const tokenNum = parseInt(searchParams.tokenId)
+
+    return {
+      title: `Logo #${tokenNum}`,
+      id: { tokenId: searchParams.tokenId },
+      media: [{ gateway: `https://storageapi.fleek.co/19601a7e-4370-48a9-9b52-d66cb11fb2e5-bucket/fakeme3nft/${tokenNum}.jpg` }]
+    }
+  }
+}
+
+const defaultMockNfts = [
+  {
+    url: 'https://eth-mainnet.alchemyapi.io/v2/abc123/getNFTs?owner=0xb25205ca60f964d45b30e969dc3f10a5de4ec3bc',
+    method: 'GET',
+    status: 200,
+    response: {
+      ownedNfts: [
+        {
+          contract: { address: '0x9759226B2F8ddEFF81583e244Ef3bd13AAA7e4A1' },
+          id: { tokenId: '0x1337' }
+        },
+        {
+          contract: { address: '0x9759226B2F8ddEFF81583e244Ef3bd13AAA7e4A1' },
+          id: { tokenId: '0x1338' }
+        },
+        {
+          contract: { address: '0x9759226B2F8ddEFF81583e244Ef3bd13AAA7e4A1' },
+          id: { tokenId: '0x1339' }
+        },
+      ]
+    }
+  },
+  metadataMock
+]
+
+faker.makeInitialRequestMap(defaultMockNfts)
 
 const containerStyle = css`
   width: 100%;
@@ -49,7 +98,7 @@ const HomePage = () => {
             component="me3-claim-widget"
             domain="me3.eth"
             provider={window.ethereum}
-            alchemyapi={{ key: 'demo', env: 'mainnet' }}
+            alchemyapi={{ key: 'abc123', env: 'mainnet' }}
             token-address="0x9759226B2F8ddEFF81583e244Ef3bd13AAA7e4A1"
             />
         </div>
