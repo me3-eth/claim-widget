09bccf67109f68319546fd9f211a3af50c3b9095
charchar <87212793+0xcharchar@users.noreply.github.com>
2023-01-07 05:29:20 +0000

    refactor: add storybook for wc, demo stories

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,11 +1,24 @@
 module.exports = {
   "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
   ],
-  "framework": "@storybook/web-components"
+  "framework": "@storybook/web-components",
+  core: {
+    disableTelemetry: false
+  },
+  webpackFinal: async (config) => {
+    // find web-components rule for extra transpilation
+    const webComponentsRule = config.module.rules.find(
+      (rule) => rule.use && rule.use.options && rule.use.options.babelrc === false
+    );
+    // add your own `my-library`
+    webComponentsRule.test.push(new RegExp(`node_modules(\\/|\\\\)me3-claim-widget(.*)\\.js$`));
+
+    return config;
+  }
 }
